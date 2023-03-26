9df2c84c86bf9e31f63d16bbd45c07b8ace1f8a0
256po <paul.bewayo4@gmail.com>
2023-03-26 01:29:15 -0700

    it-s full

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 # manifest to create file in tmp directory
-file { '/tmp/holberton':
+file { '/tmp/school':
   ensure  => file,
-  path    => '/tmp/holberton',
+  path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
